45b0bae0eaeb05f8c13907a2490eda5d35b484f6
wangdou <805677461@qq.com>
2023-01-18 20:36:14 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2020 Zac Sweers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import org.jetbrains.dokka.gradle.DokkaTask
+import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+  dependencies {
+    classpath(kotlin("gradle-plugin", version = Dependencies.Kotlin.version))
+  }
+}
+
+plugins {
+  id("com.google.devtools.ksp") version Dependencies.Kotlin.Ksp.version apply false
+  kotlin("jvm") version Dependencies.Kotlin.version apply false
+  id("org.jetbrains.dokka") version Dependencies.Kotlin.dokkaVersion  apply false
+}
+
+subprojects {
+  repositories {
+    mavenCentral()
+    google()
+  }
+
+  pluginManager.withPlugin("java") {
+    configure<JavaPluginExtension> {
+      toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+      }
+    }
+
+    project.tasks.withType<JavaCompile>().configureEach {
+      options.release.set(11)
+    }
+  }
+
+  pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
+    configure<KotlinProjectExtension> {
+      explicitApi()
+    }
+
+    tasks.withType<KotlinCompile>().configureEach {
+      kotlinOptions {
+        jvmTarget = Dependencies.Kotlin.jvmTarget
+        @Suppress("SuspiciousCollectionReassignment")
+        freeCompilerArgs += Dependencies.Kotlin.defaultFreeCompilerArgs
+      }
+    }
+
+  }
+}
